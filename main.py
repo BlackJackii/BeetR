a12113b2f64a829ecfbfb8393d3b897a469ff04e
BlackJackii <94617051+BlackJackii@users.noreply.github.com>
2021-11-20 02:28:40 +0200

    task 3

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 elem1 = "#"*9+"\n"
 elem2 = "#"+" "*7+"#"+"\n"
-print(f"{elem1 + elem2*3 + elem1}" + "\n" + f"{elem2*2 + elem1 + elem2*2}")
+print(elem1 + elem2*3 + elem1 + "\n" + elem2*2 + elem1 + elem2*2)
